4ded36882d90bd98e476e6566d63d32521fffc07
wzjq2000 <wzjq2000@163.com>
2023-12-29 17:02:47 +0800

    此分支用于学习mybatis

--- client-demo/src/test/java/com/example/clientdemo/ClientDemoApplicationTests.java
+++ client-demo/src/test/java/com/example/clientdemo/ClientDemoApplicationTests.java
@@ -1,6 +1,5 @@
 package com.example.clientdemo;
 
-import com.example.clientdemo.config.RestTemplateConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,16 +8,5 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootTest
 class ClientDemoApplicationTests {
 
-    @Autowired
-    RestTemplate restTemplate;
-    @Test
-    void restTemplateTest() {
-        String result = restTemplate.getForObject("http://localhost:8080", String.class, "");
-        System.out.println(result);
-    }
-
-    @Test
-    void restTemplateTest2(){
-    }
 
 }

8c145859b6db0a9a4965bea0939b82216469df5e
wzjq2000 <wzjq2000@163.com>
2024-01-13 18:16:13 +0800

    为了理解HandlerAdapter而写了OldVersionController

--- server-demo/src/main/java/com/example/serverdemo/ServerDemoApplication.java
+++ server-demo/src/main/java/com/example/serverdemo/ServerDemoApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ServerDemoApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ServerDemoApplication.class, args);
     }
--- server-demo/src/main/java/com/example/serverdemo/controller/RestTemplateController.java
+++ server-demo/src/main/java/com/example/serverdemo/controller/RestTemplateController.java
@@ -10,8 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/")
 public class RestTemplateController {
     @GetMapping("/")
-    public String solveIndex(@RequestHeader("key") String testId){
-        System.out.println(testId);
+//    public String solveIndex(@RequestHeader("key") String testId){
+    public String solveIndex(){
         return "THIS IS INDEX";
     }
 

979ed6420dad3e44b8a607d0568786ddde412bf4
wzjq2000 <wzjq2000@163.com>
2024-01-15 18:26:15 +0800

    .

--- server-demo/src/main/java/com/example/serverdemo/controller/RestTemplateController.java
+++ server-demo/src/main/java/com/example/serverdemo/controller/RestTemplateController.java
@@ -1,5 +1,7 @@
 package com.example.serverdemo.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -9,8 +11,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/")
 public class RestTemplateController {
+
+    private Logger logger = LoggerFactory.getLogger(RestTemplateController.class);
     @GetMapping("/")
     public String solveIndex(@RequestHeader("key") String testId){
+        logger.info("zzz");
         System.out.println(testId);
         return "THIS IS INDEX";
     }

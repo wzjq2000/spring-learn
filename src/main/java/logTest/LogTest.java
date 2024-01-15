6f473371993fc1c96811bb31f3a041b2c450138c
wzjq2000 <wzjq2000@163.com>
2024-01-15 11:17:11 +0800

    稍微理解了一下MDC,结果发现它只是一个ThreadLocal

--- src/main/java/logTest/LogTest.java
+++ src/main/java/logTest/LogTest.java
@@ -2,12 +2,14 @@ package logTest;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
 
 
 public class LogTest {
     private static final Logger logger = LoggerFactory.getLogger(LogTest.class);
 
     public static void main(String[] args) {
+//        MDC.put("k1","v1");
         logger.info("hello");
     }
 }

ac323a093d16da66eb83a11e026ae7b40c6b8506
wzjq2000 <wzjq2000@163.com>
2024-01-17 19:55:45 +0800

    添加了cglib的学习内容(enhance包下)

---
+++ server-demo/src/main/java/com/example/serverdemo/enhance/ToBeEnhanced.java
@@ -0,0 +1,10 @@
+package com.example.serverdemo.enhance;
+
+public class ToBeEnhanced {
+    public void funcA(){
+        System.out.println("A");
+    }
+    public void funcB(){
+        System.out.println("B");
+    }
+}
